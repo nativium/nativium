88138ae47b69814977190e125e992751fbb37ebe
Paulo Coutinho <paulocoutinhox@gmail.com>
2022-06-27 22:32:03 -0300

    add support for custom arch and build type for setup verb

--- core/util.py
+++ core/util.py
@@ -1,5 +1,6 @@
 import os
 
+from pygemstones.system import env as e
 from pygemstones.type import list as l
 
 from core import const
@@ -21,4 +22,50 @@ def get_version(params, config):
 # -----------------------------------------------------------------------------
 def remove_sdkroot_from_env():
     if const.REMOVE_SDKROOT_FROM_ENV:
-        os.environ.pop("SDKROOT", None)
+        e.remove_var("SDKROOT")
+
+
+# -----------------------------------------------------------------------------
+def get_parsed_arch_list(params, target_config):
+    param_archs = l.get_arg_list_values(params["args"], "--arch")
+    target_archs = target_config["archs"] if "archs" in target_config else None
+
+    if not param_archs:
+        return target_archs
+
+    if not target_archs:
+        return None
+
+    archs = []
+
+    for param_arch in param_archs:
+        for target_arch in target_archs:
+            arch_name = target_arch["arch"]
+
+            if arch_name.lower() == param_arch.lower():
+                archs.append(target_arch)
+
+    return archs
+
+
+# -----------------------------------------------------------------------------
+def get_parsed_build_type_list(params, target_config):
+    param_build_types = l.get_arg_list_values(params["args"], "--build")
+    target_build_types = (
+        target_config["build_types"] if "build_types" in target_config else None
+    )
+
+    if not param_build_types:
+        return target_build_types
+
+    if not target_build_types:
+        return None
+
+    build_types = []
+
+    for param_build_type in param_build_types:
+        for target_build_type in target_build_types:
+            if param_build_type.lower() == target_build_type.lower():
+                build_types.append(target_build_type)
+
+    return build_types
--- targets/android/verbs/setup.py
+++ targets/android/verbs/setup.py
@@ -4,7 +4,7 @@ from pygemstones.io import file as f
 from pygemstones.system import runner as r
 from pygemstones.util import log as l
 
-from core import const, target
+from core import const, target, util
 from targets.android.config import target as config
 
 
@@ -14,59 +14,68 @@ def run(params):
     target_name = params["target_name"]
     target_config = config.run(proj_path, target_name, params)
 
-    archs = target_config["archs"]
-    build_types = target_config["build_types"]
+    archs = util.get_parsed_arch_list(params, target_config)
+    build_types = util.get_parsed_build_type_list(params, target_config)
 
     if archs and len(archs) > 0:
-        for arch in archs:
-            for build_type in build_types:
-                l.i("Building for: {0}/{1}...".format(arch["conan_arch"], build_type))
-
-                # conan install
-                build_dir = os.path.join(
-                    proj_path,
-                    "build",
-                    target_name,
-                    build_type,
-                    arch["conan_arch"],
-                    "conan",
-                )
-
-                f.recreate_dir(build_dir)
-
-                build_profile = target.get_build_profile()
-
-                if build_profile != "default":
-                    build_profile = os.path.join(
-                        proj_path, "conan", "profiles", build_profile
+        if build_types and len(build_types) > 0:
+            for arch in archs:
+                for build_type in build_types:
+                    l.i(
+                        "Building for: {0}/{1}...".format(
+                            arch["conan_arch"], build_type
+                        )
                     )
 
-                run_args = [
-                    "conan",
-                    "install",
-                    os.path.join(
+                    # conan install
+                    build_dir = os.path.join(
                         proj_path,
+                        "build",
+                        target_name,
+                        build_type,
+                        arch["conan_arch"],
                         "conan",
-                        "recipe",
-                        const.FILE_NAME_CONANFILE_PY,
-                    ),
-                    "-pr:b",
-                    build_profile,
-                    "-pr:h",
-                    os.path.join(proj_path, "conan", "profiles", arch["conan_profile"]),
-                    "-s:h",
-                    "os.api_level={0}".format(arch["api_level"]),
-                ]
+                    )
 
-                target.add_target_setup_common_args(
-                    run_args, target_name, target_config, arch, build_type
-                )
+                    f.recreate_dir(build_dir)
 
-                run_args.append("--build=missing")
-                run_args.append("--update")
+                    build_profile = target.get_build_profile()
 
-                r.run(run_args, build_dir)
+                    if build_profile != "default":
+                        build_profile = os.path.join(
+                            proj_path, "conan", "profiles", build_profile
+                        )
 
-        l.ok()
+                    run_args = [
+                        "conan",
+                        "install",
+                        os.path.join(
+                            proj_path,
+                            "conan",
+                            "recipe",
+                            const.FILE_NAME_CONANFILE_PY,
+                        ),
+                        "-pr:b",
+                        build_profile,
+                        "-pr:h",
+                        os.path.join(
+                            proj_path, "conan", "profiles", arch["conan_profile"]
+                        ),
+                        "-s:h",
+                        "os.api_level={0}".format(arch["api_level"]),
+                    ]
+
+                    target.add_target_setup_common_args(
+                        run_args, target_name, target_config, arch, build_type
+                    )
+
+                    run_args.append("--build=missing")
+                    run_args.append("--update")
+
+                    r.run(run_args, build_dir)
+
+            l.ok()
+        else:
+            l.e('Build type list for "{0}" is invalid or empty'.format(target_name))
     else:
         l.e('Arch list for "{0}" is invalid or empty'.format(target_name))
