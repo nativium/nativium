fdd2e9127067962b65752d48b2817fe4b30c76dc
Paulo Coutinho <paulocoutinhox@gmail.com>
2022-07-12 01:11:08 -0300

    example of module config added

--- modules/app-core/config/module_conan.py
+++ modules/app-core/config/module_conan.py
@@ -3,9 +3,15 @@ from conans import ConanFile
 
 # -----------------------------------------------------------------------------
 def configure(params={}):
+    # example:
+    # conanfile: ConanFile = params["conanfile"]
+    # conanfile.options["openssl"].xyz = True
     pass
 
 
 # -----------------------------------------------------------------------------
 def requirements(params={}):
+    # example:
+    # conanfile: ConanFile = params["conanfile"]
+    # conanfile.requires("openssl/1.1.1o")
     pass
