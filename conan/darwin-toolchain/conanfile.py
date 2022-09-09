3a28357069b2b832de688a60ace263be7c838790
Paulo Coutinho <paulocoutinhox@gmail.com>
2022-09-09 11:26:31 -0300

    support conan v2

--- conan/darwin-toolchain/conanfile.py
+++ conan/darwin-toolchain/conanfile.py
@@ -3,7 +3,7 @@ from conan.tools import apple
 from conan.errors import ConanInvalidConfiguration
 
 
-required_conan_version = ">=1.51.3"
+required_conan_version = ">=1.52.0"
 
 
 class DarwinToolchainConan(ConanFile):
