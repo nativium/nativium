cfdf4c0ac712c2e50ddaf91590d5bdabea7474f5
Paulo Coutinho <paulocoutinhox@gmail.com>
2022-09-24 14:15:35 -0300

    support conan v2 in test package

--- conan/darwin-toolchain/test_package/conanfile.py
+++ conan/darwin-toolchain/test_package/conanfile.py
@@ -1,28 +1,40 @@
-from conans.model.conan_file import ConanFile
-from conans import CMake
 import os
 
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
 
-class DefaultNameConan(ConanFile):
-    name = "DefaultName"
-    version = "1.0.0"
-    settings = "os", "compiler", "arch", "build_type"
-    generators = "cmake"
+
+class HelloTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    # VirtualBuildEnv and VirtualRunEnv can be avoided if "tools.env.virtualenv:auto_use" is defined
+    # (it will be defined in Conan 2.0)
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualBuildEnv", "VirtualRunEnv"
+    apply_env = False
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
+    def layout(self):
+        cmake_layout(self)
+
     def test(self):
-        cmd = ".%sbin%shello" % (os.sep, os.sep)
         if self.settings.os != "Macos":
             # Ensure it fails
             try:
-                self.run(cmd)
+                self.run_tests()
             except:
                 pass
             else:
                 raise Exception("Cross building failed!")
         else:
-            self.run(cmd)
+            self.run_tests()
+
+    def run_tests(self):
+        cmd = os.path.join(self.cpp.build.bindirs[0], "example")
+        self.run(cmd, env="conanrun")
